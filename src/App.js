c60a99c07301059461643dc0f5e35e648144a7b1
Giuliano Cernada <giuliano.cernada@hotmail.com>
2019-10-17 13:27:35 -0300

    Added sidebar component but still hidden. Have to add show/hide function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './Components/Navbar/Navbar'
+import Sidebar from './Components/Sidebar/Sidebar'
 import './App.css';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Sidebar />
     </div>
   );
 }
---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Sidebar.css';
+
+const Sidebar = () => {
+	return (
+		<div className='sidebar'>
+			<ul>
+				<li>123</li>
+				<li>123</li>
+				<li>123</li>
+			</ul>
+		</div>
+	)
+}
+
+export default Sidebar;
